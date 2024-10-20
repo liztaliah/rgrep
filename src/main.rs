f609bbd89e661c1606589e570404fb8ae0c44b82
liztaliah <liz.t.hardee@gmail.com>
2024-10-20 12:01:24 -0700

    added regex matching, basic functionality working

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
-use std::fs::File;
-use std::io::{self, Read};
+use std::fs::read_to_string;
+use std::io::{self};
 use clap::Parser;
-use std::path::Path;
+use regex::Regex;
 
 #[derive(Parser)]
 
 struct Cli {
     #[arg(default_value = "", short, long, help = "enter file to read from")]
     file: String,
+    query: String,
 }
 
 fn main() {
-    let lines = io::stdin().lines();
-    // this gets stdin from pipeline
-    for line in lines {
-        println!("{}", line.unwrap());
-    }
     let cli = Cli::parse();
-
+    let re = Regex::new(&cli.query).unwrap();
+    let lines = io::stdin().lines();
     if cli.file != "" {
-        let path = Path::new(&cli.file);
-        // let display = path.display();
-
-        let mut file = File::open(&path).unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        println!("{}", contents);
+        for line in read_to_string(cli.file).unwrap().lines() {
+            if re.is_match(&line) {
+                println!("{}", line);
+            }
+        }
+        // let path = Path::new(&cli.file);
+        // let mut file = File::open(&path).unwrap();
+        // let mut contents = String::new();
+        // file.read_to_string(&mut contents).unwrap();
+        // println!("{}", contents);
+    } else {
+        // this gets stdin from pipeline
+        for line in lines {
+            if re.is_match(line.as_ref().unwrap().as_str()) {
+                println!("{}", line.unwrap());
+            }
+        }
     }
 }
